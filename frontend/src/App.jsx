0eeb72e6dae7fa6d9f5805311881651deb05bba7
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-04-25 19:19:19 +0530

    Make navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      dgsgdg
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/collection' element={<Collection />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </div>
   )
 }
